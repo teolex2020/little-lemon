dd6e84ed4410b0f82f25c7dc151bab8cd89f7df2
Oleksand Tepliuk <teolex2017@gmail.com>
2023-01-06 17:11:48 +0200

    create project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+     <header>header</header>
+     <nav>nav</nav>
+     <main>main</main>
+     <footer>footer</footer>
+    </div>
+  )
+}
+
+export default App
